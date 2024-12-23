83250ca171e348475046277a7e4ae0bd4e623c89
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2024-12-23 18:16:59 +0800

    重命名

--- src/autojs_py/opearation.py
+++ src/autojs_py/opearation.py
@@ -4,6 +4,8 @@ from capture import screenshot
 from reconginze import template_match
 import time
 
+from src.autojs_py.ocr import ocr2box
+
 
 def Click(x,y):
     pyautogui.click(x,y)
@@ -21,7 +23,7 @@ def LongClick(s):
     pyautogui.mouseDown()
     time.sleep(s)
     pyautogui.mouseUp()
-def Click(template: str) -> None:
+def tclick(template: str) -> None:
     matching_positions, image = template_match(screenshot(), template, threshold=0.8)
 
     if len(matching_positions) == 0:
@@ -31,7 +33,7 @@ def Click(template: str) -> None:
     pos = matching_positions[0]
     x, y = pos[0] + pos[2] / 2, pos[1] + pos[3] / 2
     pyautogui.click(x, y)
-def LClick(template: str) -> None:
+def tlclick(template: str) -> None:
     matching_positions, image = template_match(screenshot(), template, threshold=0.8)
 
     if len(matching_positions) == 0:
@@ -41,7 +43,7 @@ def LClick(template: str) -> None:
     pos = matching_positions[0]
     x, y = pos[0] + pos[2] / 2, pos[1] + pos[3] / 2
     pyautogui.leftClick(x, y)
-def RClick(template: str) -> None:
+def trcick(template: str) -> None:
     matching_positions, image = template_match(screenshot(), template, threshold=0.8)
 
     if len(matching_positions) == 0:
@@ -52,7 +54,7 @@ def RClick(template: str) -> None:
     x, y = pos[0] + pos[2] / 2, pos[1] + pos[3] / 2
     pyautogui.rightClick(x, y)
 
-def DClick(template: str) -> None:
+def dclick(template: str) -> None:
     matching_positions, image = template_match(screenshot(), template, threshold=0.8)
 
     if len(matching_positions) == 0:
@@ -67,7 +69,7 @@ def Swap(pos: list[int], pos2: list[int]) -> None:
     pyautogui.moveTo(pos[0], pos[1], 0.5)
     pyautogui.dragTo(pos2[0], pos2[1], 0.5)
 
-def Swap(template:str,direct:list[int])->None:
+def tswap(template:str,direct:list[int])->None:
     matching_positions, image = template_match(screenshot(), template, threshold=0.8)
     if len(matching_positions) == 0:
         raise ValueError("No matching positions found")
@@ -75,3 +77,23 @@ def Swap(template:str,direct:list[int])->None:
     x, y = pos[0] + pos[2] / 2, pos[1] + pos[3] / 2
     pyautogui.moveTo(x, y, 0.5)
     pyautogui.dragRel(direct[0], direct[1], 0.5)
+
+def clicktext(text:str):
+    shot = screenshot()
+    data = ocr2box(shot)
+    # 遍历识别到的文本
+    for i in range(len(data['text'])):
+        if int(data['conf'][i]) > 60:  # 过滤低置信度文本
+            text = data['text'][i]
+            x = data['left'][i]
+            y = data['top'][i]
+            w = data['width'][i]
+            h = data['height'][i]
+
+            # 计算文本中心位置
+            center_x = x + w // 2
+            center_y = y + h // 2
+
+            # 点击识别到的文字位置
+            pyautogui.click(center_x, center_y)
+            break
